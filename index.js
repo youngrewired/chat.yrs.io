0367f3e7bf7e2c9f6a3a3a140d60ddb388b55815
Luca Marchal <lucamarchal@gmail.com>
2015-07-23 20:56:44 +0300

    Temp spam protection

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var sanitizeHtml = require('sanitize-html');
-
 app.use(express.static(__dirname));
+var list = require('badwords-list');
+banned = list.array;
 
 app.get('/', function(req, res){
   res.sendFile('/index.html');
@@ -15,6 +16,21 @@ io.on('connection', function(socket){
   	if(msg == '' || msg == undefined || msg == null) {
   		return;
   	}
+
+  	var allowed = true;
+  	var m = msg.split(' ');
+  	m.forEach(function(msg) {
+  		banned.forEach(function(word) {
+  			if(wordInString(msg, word)) {
+  				allowed = false;
+  			}
+  		});
+  	});
+
+  	if(allowed == false) {
+  		return;
+  	}
+
 		var clean = sanitizeHtml(msg, {
 		  allowedTags: [ 'b', 'i', 'em', 'strong', 'a' ],
 		  allowedAttributes: {
@@ -31,6 +47,11 @@ io.on('connection', function(socket){
   });
 });
 
+function wordInString(s, word){
+  return new RegExp( '\\b' + word + '\\b', 'i').test(s);
+}
+
 http.listen(80, function(){
+	banned.push('SPAM')
   console.log('listening on Port 80');
 });
