e5a1c3e52d86be3d170f3ffc4195211094298b44
Luca Marchal <lucamarchal@gmail.com>
2015-07-23 13:00:31 +0300

    do the css

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+app.use(express.static(__dirname));
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
