8ddb0f0eef524bb9f51606c77199ee8c371ab8ce
Levi Wright <leviwright1997@googlemail.com>
2015-07-24 15:41:18 +0100

    Add firebase url config option

--- index.js
+++ index.js
@@ -3,13 +3,25 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var sanitizeHtml = require('sanitize-html');
+var fs = require('fs');
 app.use(express.static(__dirname));
 var list = require('badwords-list');
 banned = list.array;
 
-app.get('/', function(req, res){
-	res.sendFile('/index.html');
+var data = fs.readFileSync("config.json", "utf8", function(err, data) {
+	if (err) throw err;
 });
+var config = JSON.parse(data);
+
+app.get('/', function(req, res) {
+	var page = fs.readFileSync("./chat.html", "utf8", function(err, data) {
+		if (err) throw err;
+	});
+	
+	page = page.replace("***firebase-url***", config.firebase_url);
+	res.send(page);
+});
+
 
 io.on('connection', function(socket){
 	socket.on('chat message', function(msg, user){
@@ -51,7 +63,7 @@ function wordInString(s, word){
 	return new RegExp( '\\b' + word + '\\b', 'i').test(s);
 }
 
-http.listen(3000, function(){
+http.listen(config.port, function(){
 	banned.push('SPAM');
-    console.log('listening on Port 3000');
+    console.log('listening on Port ' + config.port);
 });
