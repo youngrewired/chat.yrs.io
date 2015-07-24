c909d792e5b640dd5a4196f9c9f67d1f9527d614
Levi Wright <leviwright1997@googlemail.com>
2015-07-24 15:05:09 +0100

    Make sure index.js uses TABS instead of a mix of tabs and double spaces.

--- index.js
+++ index.js
@@ -8,50 +8,50 @@ var list = require('badwords-list');
 banned = list.array;
 
 app.get('/', function(req, res){
-  res.sendFile('/index.html');
+	res.sendFile('/index.html');
 });
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg, user){
-  	if(msg == '' || msg == undefined || msg == null) {
-  		return;
-  	}
+	socket.on('chat message', function(msg, user){
+		if(msg == '' || msg == undefined || msg == null) {
+			return;
+		}
 
-  	var allowed = true;
-  	var m = msg.split(' ');
-  	m.forEach(function(msg) {
-  		banned.forEach(function(word) {
-  			if(wordInString(msg, word)) {
-  				allowed = false;
-  			}
-  		});
-  	});
+		var allowed = true;
+		var m = msg.split(' ');
+		m.forEach(function(msg) {
+			banned.forEach(function(word) {
+				if(wordInString(msg, word)) {
+					allowed = false;
+				}
+			});
+		});
 
-  	if(allowed == false) {
-  		return;
-  	}
+		if(allowed == false) {
+			return;
+		}
 
 		var clean = sanitizeHtml(msg, {
-		  allowedTags: [ 'b', 'i', 'em', 'strong', 'a' ],
-		  allowedAttributes: {
-		    'a': [ 'href' ]
-		  }
+			allowedTags: [ 'b', 'i', 'em', 'strong', 'a' ],
+			allowedAttributes: {
+				'a': [ 'href' ]
+			}
 		});
 		var cleanUser = sanitizeHtml(user, {
-		  allowedTags: [ 'b', 'i', 'em', 'strong', 'a' ],
-		  allowedAttributes: {
-		    'a': [ 'href' ]
-		  }
+			allowedTags: [ 'b', 'i', 'em', 'strong', 'a' ],
+			allowedAttributes: {
+				'a': [ 'href' ]
+			}
 		});
-    io.emit('chat message', clean, cleanUser);
-  });
+		io.emit('chat message', clean, cleanUser);
+	});
 });
 
 function wordInString(s, word){
-  return new RegExp( '\\b' + word + '\\b', 'i').test(s);
+	return new RegExp( '\\b' + word + '\\b', 'i').test(s);
 }
 
 http.listen(3000, function(){
-	banned.push('SPAM')
-  	console.log('listening on Port 3000');
+	banned.push('SPAM');
+    console.log('listening on Port 3000');
 });
