0e48a2dbfd86fc4e5678b537b87b8a9ccab80206
Levi Wright <leviwright1997@googlemail.com>
2015-07-25 00:20:37 +0100

    Made all statements use showMessage when displaying messages

--- templates/chat.js
+++ templates/chat.js
@@ -17,12 +17,28 @@ $('.helpButton').click(function() {
 
 function showMessage(msg, user, imageLink) {
   msg = emojione.toImage(msg);
+
+
+
   var messageElement;
   if (user == 'RubyBot') {
     messageElement = $('<li class="bot-msg">').html('<a href="http://yrs.io">' + user + '</a>' + ': ' + msg);
-  } else{
-    messageElement = $('<li>').html('<a href="https://twitter.com/'+ user +'" target="_blank"><img class="profileImage" src="' + imageLink + '"/></a><div class="message"><a class="twitter-link" href="https://twitter.com/'+ user +'" target="_blank">@' + user + '</a>' + ': ' + msg + '</div>');
+
+  } else if (user == 'Server') {
+    messageElement = $('<li class="server-msg">').html('<a href="http://yrs.io">' + user + '</a>' + ': ' + msg);
+
+  } else {
+
+    if (!imageLink || imageLink == ''){
+      console.log("1");
+      messageElement = $('<li>').html('<a href="https://twitter.com/'+ user +'" target="_blank"></a><div class="message"><a class="twitter-link" href="https://twitter.com/'+ user +'" target="_blank">@' + user + '</a>' + ': ' + msg + '</div>');
+    } else {
+      console.log("2");
+      messageElement = $('<li>').html('<a href="https://twitter.com/'+ user +'" target="_blank"><img class="profileImage" src="' + imageLink + '"/></a><div class="message"><a class="twitter-link" href="https://twitter.com/'+ user +'" target="_blank">@' + user + '</a>' + ': ' + msg + '</div>');
+    }
+
   }
+
   $('#messages').append(messageElement).animate({scrollTop: 1000000}, "slow");
   $('li').linkify({
     target: "_blank"
@@ -52,15 +68,13 @@ ref.getAuth();
 
 $('form').submit(function(){
   if(canPost == false){
-
-    var console = $('<li>').html('<span><font color="red">Server</span>' + ': Please do not spam!</font>')
-    $('#messages').append(console).animate({scrollTop: 1000000}, "slow");
-
+    showMessage('Please do not spam!', 'Server', '');
   } else {
     socket.emit('chat message',
       msgbox.val(),
       authData.twitter.username,
-      authData.twitter.profileImageURL);
+      authData.twitter.profileImageURL
+    );
     canPost=false;
     setTimeout(function(){canPost=true},500);
   }
@@ -74,4 +88,5 @@ socket.on('chat message', showMessage);
 window.onbeforeunload = function(e) {
   return 'Closing YRS Chat means you will no longer receive messages';
 };
+
 });
